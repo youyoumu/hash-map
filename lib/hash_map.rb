f0118a9d36da09c47fbc10e8f429fb192dc361a1
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-01-13 14:59:55 +0700

    Create #hash

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -2,4 +2,13 @@ class HashMap
   def initialize(size = 16)
     @buckets = Array.new(size)
   end
+
+  def hash(value)
+    result = 0
+    value.each do |char|
+      result += char.ord
+    end
+    result
+  end
+
 end
